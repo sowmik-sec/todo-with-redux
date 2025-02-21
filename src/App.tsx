edc9abc059f555cf1f45d6344cb4881b95d7320a
sowmik-sec <sowmik.sec@gmail.com>
2025-02-21 16:27:52 +0600

    projet initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Hello world!</h2>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
