f3ef70ba71efe81576dd742924ff22b35efc8962
sowmik-sec <sowmik.sec@gmail.com>
2025-02-21 17:38:20 +0600

    Install Shadcn components

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,19 +1,23 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add Todo</button>
-        <button>Filter</button>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal />
+        <Button>Filter</Button>
       </div>
-      <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-3">
+      <div className="bg-red-500 bg-primary-gradient p-1 w-full h-full rounded-xl">
         {/* <div className="bg-white p-5 text-2xl font-bold flex justify-center items-center rounded-md ">
           There is not task pending
         </div> */}
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
+        <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    backgroundImage: {
+      "primary-gradient": "linear-gradient(30deg, #DC02C3, #5C53FE)",
+    },
+  },
 };
 export const plugins = [];
